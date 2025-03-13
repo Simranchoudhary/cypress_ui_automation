8c54d25c634ad81f8b7c21883f75162bd2317f42
Simran Choudhary <simranchoudhary@Simrans-MacBook-Air.local>
2025-03-14 00:48:05 +0530

    Added the configuration to store reports in reports folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,17 @@
+import { defineConfig } from "cypress";
+
 export default defineConfig({
   e2e: {
     baseUrl: "https://magento.softwaretestingboard.com",
     video: true,
+    screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
 });
