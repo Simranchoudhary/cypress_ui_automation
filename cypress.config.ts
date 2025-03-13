85af33cba65125317e5bb236756ae0cfe78de9fa
Simran Choudhary <simranchoudhary@Simrans-MacBook-Air.local>
2025-03-14 00:39:03 +0530

    updated the config file to capture videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "cypress";
-
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://magento.softwaretestingboard.com",
+    video: true,
+    videosFolder: "cypress/videos",
   },
 });
